a2b5c6561dd708e881792f93e5050d2f46db78cf
chenmingjun66 <1011693028@qq.com>
2019-11-26 14:25:08 +0800

    分布式session已经完成

---
+++ src/main/java/com/chenmingjun/miaosha/controller/GoodsController.java
@@ -0,0 +1,27 @@
+package com.chenmingjun.miaosha.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.chenmingjun.miaosha.domain.MiaoshaUser;
+import com.chenmingjun.miaosha.redis.RedisService;
+import com.chenmingjun.miaosha.service.MiaoshaUserService;
+
+@Controller
+@RequestMapping("/goods")
+public class GoodsController {
+
+	@Autowired
+	MiaoshaUserService userService;
+	
+	@Autowired
+	RedisService redisService;
+	
+    @RequestMapping("/to_list")
+    public String list(Model model,MiaoshaUser user) {
+    	model.addAttribute("user", user);
+        return "goods_list";
+    }
+}
---
+++ src/test/java/com/chenmingjun/miaoshamooc/MiaoshaMoocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenmingjun.miaoshamooc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaMoocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
